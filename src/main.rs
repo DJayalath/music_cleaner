3c167bb24a469e11057246d68d53a1b94664eae6
armytricks <dhjayalath@gmail.com>
2019-07-10 18:14:09 +1000

    Safely scanned directory

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,48 @@
 use std::fs;
 
 fn main() {
-    read_path();
+
+    // Scan files and folders in directory
+    let (files, folders) = match scan_path() {
+
+        Ok((fi, fo)) => {
+        
+            println!("\nFound {} files:\n", fi.len());
+            for f in &fi {
+                println!("{:?}", f.file_name());
+            }
+
+            println!("\nFound {} folders:\n", fo.len());
+            for f in &fo {
+                println!("{:?}", f.file_name());
+            }
+
+            (fi, fo)
+        },
+
+        Err(e) => panic!("ERROR: {}", e)
+
+    };
 }
 
-fn read_path() {
-    let paths = fs::read_dir("C:/Users/dhjay/Music/Music").unwrap();
+fn scan_path() -> Result<(Vec<fs::DirEntry>, Vec<fs::DirEntry>), std::io::Error> {
 
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
+    let paths = fs::read_dir("C:/Users/dhjay/Music/Music")?;
+
+    let mut files = Vec::new();
+    let mut folders = Vec::new();
+
+    for p in paths {
+        
+        let path = p?;
+        let metadata = path.metadata()?;
+
+        if metadata.is_file() {
+            files.push(path);
+        } else {
+            folders.push(path);
+        }
     }
+
+    Ok((files, folders))
 }
